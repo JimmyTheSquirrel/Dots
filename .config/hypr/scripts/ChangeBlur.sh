6e073b694fecbd8e55d475f9f1ab40f036832b27
Hello <emailrhysgeddes@gmail.com>
2025-07-28 20:37:00 +1000

    uploaded dots

---
+++ .config/hypr/scripts/ChangeBlur.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Script for changing blurs on the fly
+
+	hyprctl keyword decoration:blur:size 2
+	hyprctl keyword decoration:blur:passes 1
+
+
