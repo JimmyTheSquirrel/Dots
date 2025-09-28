e29afe49e3650f38987f3ac90965afb3212489fb
Rhys <Emailrhysgeddes@gmail.com>
2025-09-28 21:33:30 +1000

    sync config

---
+++ Modules/Brave.nix
@@ -0,0 +1,19 @@
+# Brave.nix
+{ config, pkgs, ... }:
+{
+  programs.brave = {
+    enable = true;
+    extensions = [
+      { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
+    ];
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/http" = [ "brave-browser.desktop" ];
+      "x-scheme-handler/https" = [ "brave-browser.desktop" ];
+    };
+  };
+}
