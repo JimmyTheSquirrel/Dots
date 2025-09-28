2370deb4e00a2721132ed593f9f7b8b7de56d5cf
Rhys <Emailrhysgeddes@gmail.com>
2025-09-28 20:03:42 +1000

    removed all shit

--- .config/hypr/scripts/ChangeBlur.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-# Script for changing blurs on the fly
-
-	hyprctl keyword decoration:blur:size 2
-	hyprctl keyword decoration:blur:passes 1
-
-
