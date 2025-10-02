2482dae831e7536e53704e83118e98bfb3191db8
Rhys <Emailrhysgeddes@gmail.com>
2025-10-02 13:27:37 +1000

    Project-Base Stable 22

--- Modules/WallpaperCarousel.nix
+++ Modules/WallpaperCarousel.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 
 let
-  scriptDir = ../../Dots/Modules/Scripts;
-in
-{
+  scriptsDir = ./Scripts;
+in {
+  # Required programs
   home.packages = with pkgs; [
     rofi
     swww
@@ -12,24 +12,28 @@ in
     pywal
   ];
 
-  home.file."scripts/wallpaper_selector.sh".source = "${scriptDir}/wallpaper_selector.sh";
+  # Wallpaper script
+  home.file."scripts/wallpaper_selector.sh".source = scriptsDir + "/wallpaper_selector.sh";
 
-  home.file."config/waypaper/config.ini".source = "${scriptDir}/config.ini";
+  # Rofi theme file
+  home.file.".config/rofi/wallpaper-sel-config.rasi".source = scriptsDir + "/wallpaper-sel-config.rasi";
 
-  # Optional: make the script executable
-  home.activation.makeScriptsExecutable = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  # Stub out the pywal rofi-colors if not generated yet
+  home.file.".cache/wal/rofi-colors.rasi".text = ''
+    * {
+      background: #1e1e2e;
+      foreground: #cdd6f4;
+      selected-normal-background: #89b4fa;
+      selected-normal-foreground: #1e1e2e;
+      border-color: #f38ba8;
+    }
+  '';
+
+  # Make script executable
+  home.activation.makeWallpaperScriptExecutable = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     chmod +x "${config.home.homeDirectory}/scripts/wallpaper_selector.sh"
   '';
 
-  # Optional: alias to make it easy to call
+  # Optional: Rofi alias for quick testing
   home.shellAliases.wall = "bash ~/scripts/wallpaper_selector.sh";
-
-  # Optional: Desktop entry to launch from menu
-  home.file.".local/share/applications/WallpaperSelector.desktop".text = ''
-    [Desktop Entry]
-    Name=Wallpaper Selector
-    Exec=${config.home.homeDirectory}/scripts/wallpaper_selector.sh
-    Type=Application
-    Terminal=false
-  '';
 }
